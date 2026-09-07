3abe46effef586bc9e0f788bc8d98463550d519a
codevansh <vanshmehta0304@gmail.com>
2026-09-07 06:49:07 -0700

    day-7 work done till part-2

--- src/data_processing/ner_processor.py
+++ src/data_processing/ner_processor.py
@@ -1,7 +1,10 @@
 from pathlib import Path
 import spacy
+import json
 
 OCR_INPUT_PATH = Path("data/processed/ocr_sample.txt")
+NER_EVALUATION_PATH= Path('data/processed/ner_evaluation.json')
+
 
 def load_text(file_path):
     if not file_path.exists():
@@ -50,8 +53,41 @@ def main():
     text=load_text(OCR_INPUT_PATH)
     nlp=load_ner_model()
     entities=extract_entities(nlp,text)
+    
+    label_counts=label_entities(entities)
+    for label,count in label_counts.items():
+        print(f"{label}:{count}")
+    
     display_entities(entities)
     print("\n Baseline NER processing completed successfully")
     
+    save_ner_evaluation(entities,label_counts)
+    
+    
+# Day-7 --> NER (Named Entities Recognition)
+def label_entities(entities):
+    label_counts={}
+    for i in entities:
+        label = i['label']
+        
+        if label in label_counts:
+            label_counts[label]+=1
+        else:
+            label_counts[label]=1
+    return label_counts
+
+
+def save_ner_evaluation(entities,label_counts):
+    NER_EVALUATION_PATH.parent.mkdir(parents=True,exist_ok=True)
+    
+    evaluation = {
+        'total_entities':len(entities),
+        'label_counts':label_counts
+    }
+    
+    with open(NER_EVALUATION_PATH,'w',encoding='utf-8') as file:
+        json.dump(evaluation,file,indent=4)
+    print(f"NER evaluation saved to: {NER_EVALUATION_PATH}")
+
 if __name__ == '__main__':
     main()
